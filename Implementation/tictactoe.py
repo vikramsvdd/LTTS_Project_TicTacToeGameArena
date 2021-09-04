2feaa83f79018f00c5fc768effe7a59327a22f97
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2021-09-04 11:35:16 +0530

    Update tictactoe.py

--- Implementation/tictactoe.py
+++ Implementation/tictactoe.py
@@ -28,6 +28,9 @@ def main():
         if game_board.is_winner(current_player):  # First check for victory.
             print(game_board.get_board_str())
             print(current_player + ' has won the game!')
+            break
+            
+            
         elif game_board.is_board_full():  # Next check for a tie.
             print(game_board.get_board_str())
             print('The game is a tie!')
